dba88941535a827d8528ba398fa2bc23bc9b0c88
kutta97 <yhjin04170@gmail.com>
2021-01-24 02:38:33 +0900

    add ft_memchr() function prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 00:28:24 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 02:18:40 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 02:38:01 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
