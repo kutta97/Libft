f8a7584f44e142e05a5b4094eac6dcaf1e2c3df3
kutta97 <yhjin04170@gmail.com>
2021-01-24 01:18:21 +0900

    add bzero() function prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,15 +6,16 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 00:28:24 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 00:44:50 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 01:12:18 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdlib.h>
+#include <stdlib.h>
 
 void	*ft_memset(void *b, int c, size_t len);
+void	bzero(void *s, size_t n);
 
 #endif
