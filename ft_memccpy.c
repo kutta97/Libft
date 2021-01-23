08402ce9d5db895264682ac589e9aad45c4c2dcf
kutta97 <yhjin04170@gmail.com>
2021-01-24 01:53:22 +0900

    write ft_memccpy() function and modify variable name in ft_memcpy() to be same with ft_memeccpy()

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,8 +6,20 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 01:46:34 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 01:46:36 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 01:51:16 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char *s;
+	int i;
+
+	s = src;
+	i = 0;
+	while (i < n && s[i] != c)
+		memset(dst, s[i++], 1);
+	return (dst);
+}
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 01:26:08 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 01:41:19 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 01:50:15 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char *c;
+	unsigned char *s;
 	int i;
 
-	c = src;
+	s = src;
 	i = 0;
 	while (i < n)
-		memset(dst, c[i++], 1);
+		memset(dst, s[i++], 1);
 	return (dst);
 }
