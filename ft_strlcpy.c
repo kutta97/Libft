3eafa8302051500fc8e39ba1a2cf695bc5af2094
kutta97 <yhjin04170@gmail.com>
2021-01-24 08:01:36 +0900

    write ft_strlcpy()

---
+++ ft_strlcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/24 07:57:46 by hyyang            #+#    #+#             */
+/*   Updated: 2021/01/24 08:01:06 by hyyang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (dstsize == 0)
+		return (src_len);
+	while (--dstsize && *src)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (src_len);
+}
