10583d872cff63cb06d96043c31b2fe53b99306c
kutta97 <yhjin04170@gmail.com>
2021-01-24 01:55:30 +0900

    add ft_memmove() function prototpye in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 00:28:24 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 01:45:29 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 01:54:58 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
