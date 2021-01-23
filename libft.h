abe76d764c0d10233ce3d040711a8ad93fe6ae6f
kutta97 <yhjin04170@gmail.com>
2021-01-24 00:45:27 +0900

    add ft_memset() function prototype in libft.h

--- libft.h
+++ libft.h
@@ -6,8 +6,15 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 00:28:24 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 00:28:30 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 00:44:50 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
 
+# include <stdlib.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
