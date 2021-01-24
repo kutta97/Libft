f3199402cfd00c23a547f164eec404bafe0bfda5
kutta97 <yhjin04170@gmail.com>
2021-01-24 17:45:45 +0900

    modified ft_memcmp to remove duplicating return values

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 07:34:54 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 07:39:08 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/24 17:41:28 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	while (--n)
+	while (n--)
 	{
 		if (*p1 != *p2)
 			return (*p1 - *p2);
 		p1++;
 		p2++;
 	}
-	return (*p1 = *p2);
+	return (0);
 }
