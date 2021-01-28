5e2071523184ae4ad4c1ae50b0234d42d4d705fe
kutta97 <yhjin04170@gmail.com>
2021-01-29 01:45:48 +0900

    modify ft_strjoin, add condition when s1 or s2 is null pointer

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 20:02:22 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/29 00:32:43 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/29 01:45:04 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 || !s2)
+		return (!s1 ? ft_strdup(s2) : ft_strdup(s1));
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	if (!(str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char))))
