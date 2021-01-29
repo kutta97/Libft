c1192519000dccbf5e237bb095d161a191a262a3
kutta97 <yhjin04170@gmail.com>
2021-01-29 16:26:24 +0900

    add conditions for Null params

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hyyang <hyyang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 01:26:08 by hyyang            #+#    #+#             */
-/*   Updated: 2021/01/24 06:00:17 by hyyang           ###   ########.fr       */
+/*   Updated: 2021/01/29 05:06:57 by hyyang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*s;
+	size_t			i;
 
+	if (!dst && !src)
+		return (0);
 	s = (unsigned char *)src;
-	while (n--)
-		ft_memset(dst, *s++, 1);
+	i = 0;
+	while (i < n)
+	{
+		ft_memset(&dst[i], s[i], 1);
+		i++;
+	}
 	return (dst);
 }
